5900f88deb60021b6ac73100e7195ff79c013112
Grant Robinson <Chisdeal2014@hotmail.com>
2022-01-23 12:57:44 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+module.exports.UrlToIPFS = function(url) {
+  
+  const { create, urlSource, ipfsHttpClient } = require('ipfs-http-client')
+  const ipfs = create('https://ipfs.infura.io:5001/api/v0')
+
+  if (!url) {
+    console.error('expected a url as argument')
+    process.exit(1)
+  }
+
+  async function UrlToIPFS(url) {
+
+    const file = await ipfs.add(urlSource(url));
+
+    var cid = 'https://ipfs.infura.io/ipfs/'+file.cid;
+
+    return {'path': file.path, 'cid': file.cid, 'size': file.size, 'url': cid};
+
+  }
+
+  UrlToIPFS(url).then(data => {
+	    return data;
+  });
+  
+}
